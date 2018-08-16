d07535f71cf7cae37467f18e009d56a4236cee75
abhishek <lsingh@gmail.com>
2018-08-16 13:26:51 +0530

    new method added

--- src/main/java/com/git/Dummy.java
+++ src/main/java/com/git/Dummy.java
@@ -3,7 +3,15 @@ package com.git;
 public class Dummy {
 	public static void main( String[] args )
     {
-        System.out.println( "Hello Dummy!" );
+        System.out.println( "Hello manish!" );
     }
+	void m2(){
+
+		System.out.println("m2");
+	}
+	void m3(){
+
+		System.out.println("m2");
+	}
 
 }
